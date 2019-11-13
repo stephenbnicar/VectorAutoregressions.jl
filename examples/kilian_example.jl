b699bc2d80cead20ac1a0df17865a985086ed6ad
Stephen Nicar <stephenbnicar@gmail.com>
2019-11-13 17:32:43 -0600

    Updated lagselect function, kilian example

--- examples/kilian_example.jl
+++ examples/kilian_example.jl
@@ -9,11 +9,17 @@ cd(@__DIR__)
 data_df = CSV.read("kilian_data.csv")
 data_mat = Matrix(data_df)
 
-# TimeArray
+# Construct TimeArray from Data
 dates = collect(Date(1954, 12, 1):Dates.Month(3):Date(2007, 12, 1))
 data_df.timestamp = dates
 data_ts = TimeArray(data_df, timestamp = :timestamp)
 
-var_mat = VAR(data_mat, 4)
-var_df = VAR(data_df[!, 1:3], 4)
-var_ts = VAR(data_ts, 4)
+# Lag Selection Criteria
+ls_mat = lagselect(data_mat, 8)
+ls_df = lagselect(data_df[!, 1:3], 8)
+ls_ts = lagselect(data_ts, 8)
+
+# Estimate VAR
+var_mat = VAR(data_mat, ls_mat.selection[:AIC])
+var_df = VAR(data_df[!, 1:3], ls_df.selection[:AIC])
+var_ts = VAR(data_ts, ls_ts.selection[:AIC])
--- src/lag_selection.jl
+++ src/lag_selection.jl
@@ -1,9 +1,31 @@
+"""
+    LagSelectionCriteria
+
+Fields:
+---
+* `maxlag` : `Int`
+* `table` : `DataFrame`
+* `selction` : `Dict`
+"""
 struct LagSelectionCriteria
     maxlag::Int
     table::DataFrame
     selection::Dict
 end
 
+"""
+    lagselect(data, lags, constant::Bool = true,
+        trend::Bool = false) -> LagSelectionCriteria
+
+Calculate AIC, SIC, and HQC lag selection criteria for an unrestricted VAR.
+
+Arguments:
+---
+* `data` : `Matrix`, `DataFrame`, or `TimeArray` of observations on endogenous variables
+* `lags` : maximum number of lags
+* `constant` : boolean to indicate inclusion of intercept term (default is `true`)
+* `trend` : boolean to indicate inclusion of a linear trend
+"""
 function lagselect(
     data::Matrix,
     maxlag;
@@ -26,6 +48,26 @@ function lagselect(
     table = DataFrame(lag = collect(1:maxlag), AIC = AIC, HQC = HQC, SIC = SIC)
     selection = Dict(cn => argmin(table[!, cn]) for cn in names(table)[2:end])
     LagSelectionCriteria(maxlag, table, selection)
+end
+
+function lagselect(
+    data::DataFrame,
+    maxlag;
+    constant::Bool = true,
+    trend::Bool = false,
+)
+    data = Matrix(data)
+    lagselect(data, maxlag; constant = constant, trend = trend)
+end
+
+function lagselect(
+    data::TimeArray,
+    maxlag;
+    constant::Bool = true,
+    trend::Bool = false,
+)
+    data = DataFrame(data)[:, 2:end]
+    lagselect(data, maxlag; constant = constant, trend = trend)
 end
 
 function show(io::IO, ls::LagSelectionCriteria)
