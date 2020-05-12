fbde8230197227e687e181c283a14d597ffa66aa
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-12 14:03:25 +0000

    build based on db485cf

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#VectorAutoregressions.jl-1","page":"Home","title":"VectorAutoregressions.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"VectorAutoregressions docs.","category":"page"}]
+[{"location":"#VectorAutoregressions.jl-1","page":"Home","title":"VectorAutoregressions.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"VectorAutoregressions docs.","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Adding new stuff for a test.","category":"page"}]
 }
