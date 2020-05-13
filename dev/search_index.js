b8edd7acb8792a555a8234f8092eddfb6d1c10f1
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-13 18:02:29 +0000

    build based on 17b192d

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#VectorAutoregressions.jl-1","page":"Home","title":"VectorAutoregressions.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"VectorAutoregressions docs.","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Adding new stuff for a test.","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Adding another line for a new test.","category":"page"}]
+[{"location":"#VectorAutoregressions.jl-1","page":"Home","title":"VectorAutoregressions.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"VectorAutoregressions.jl is a package for estimating Vector Autoregressions (VARs) using Julia.  The initial goal is to provide functionality comparable to the vars package in R.","category":"page"}]
 }
