1892cedadb5785cd18dca4a99bec14d2994eed52
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-13 18:08:15 +0000

    build based on a123427

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#VectorAutoregressions.jl-1","page":"Home","title":"VectorAutoregressions.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"VectorAutoregressions.jl is a package for estimating Vector Autoregressions (VARs) using Julia.  The initial goal is to provide functionality comparable to the vars package in R.","category":"page"}]
+[{"location":"#VectorAutoregressions.jl-1","page":"Home","title":"VectorAutoregressions.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"VectorAutoregressions.jl is a package for estimating Vector Autoregressions (VARs) using Julia.  The initial goal is to provide functionality comparable to the <a href=\"https://cran.r-project.org/package=vars\" target=\"_blank\">vars</a> package in R.","category":"page"}]
 }
