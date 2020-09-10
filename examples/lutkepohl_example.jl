f142516840131ddac8b410b2024e852150aaa6e3
Stephen Nicar <stephenbnicar@gmail.com>
2020-09-10 13:16:45 -0500

    Updates to example.

--- examples/lutkepohl_example.jl
+++ examples/lutkepohl_example.jl
@@ -1,11 +1,12 @@
-# This file reproduces the example introduced in Section 3.2.3 of Lütkepohl (2006)
+# The following reproduces the example introduced in Section 3.2.3 of Lütkepohl (2006)
 
 using VectorAutoregressions
 using CSV, DataFrames, DataFramesMeta
 using Dates
 
 exdir = pkgdir(VectorAutoregressions) * "/examples";
-datadf = DataFrame!(CSV.File(exdir * "/lutkepohl_data.csv"))
+datadf = DataFrame!(CSV.File(exdir * "/lutkepohl_data.csv"));
+first(datadf, 6)
 
 # Take the subset from 1960q1 to 1978q4, and use log-difference for each series
 Y = @linq datadf |>
@@ -14,7 +15,8 @@ Y = @linq datadf |>
           investment = diff(log.(:invest)),
           income = diff(log.(:income)),
           consumption = diff(log.(:cons)),
-      )
+      );
+first(Y, 6)
 
 # ## Get lag selection criteria
 ls = LagSelection(Y, 8)
@@ -24,4 +26,3 @@ v = VAR(Y, ls.selection["AIC"])
 sc = StabilityCheck(v)
 # ## Check for autocorrelation in the residuals
 pt = PortmanteauTest(v, 12)
-bgt = bg_test(v, 12)
